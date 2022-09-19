0c93252de813e808bf77e9972acee1711e87d240
Ariel Sebastián Gatti <arielgatti444@gmail.com>
2022-09-18 22:47:04 -0300

    Class ventaRepuestos
    Primera Implementacion de Clase Venta de Repuestos

---
+++ ventaRepuestos/ventaRepuestos.Entidades/VentaRepuestos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ventaRepuestos.Entidades
+{
+    public class VentaRepuestos
+    {
+        private List<Repuesto> _listaProductos;
+        private String _nombreComercio;
+        private String _direccion;
+
+        public List<Repuesto> ListaProductos { get => _listaProductos;}
+        public string NombreComercio { get => _nombreComercio;}
+        public String Direccion { get => _direccion;}
+
+        public void AgregarRepuesto(Repuesto r)
+        {
+
+        }
+
+        public void QuitarRepuesto(int codigo)
+        {
+
+        }
+
+        public void ModificarPrecio(int codigo, double precioNuevo)
+        {
+
+        }
+
+        public void AgregarStock(int codigo, int cantidad)
+        {
+
+        }
+
+        public void QuitarStock(int codigo, int cantidad)
+        {
+
+        }
+
+        public List<Repuesto> TraerPorCategoria(int codigoCat)
+        {
+
+        }
+    }
+}
