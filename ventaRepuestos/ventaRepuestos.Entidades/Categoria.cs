6848c92771022a1bee362008cc2b7cbc0c7f2dfb
Ariel Sebastián Gatti <arielgatti444@gmail.com>
2022-09-18 22:36:42 -0300

    Actualizacion Clases Categorias y Repuestos

---
+++ ventaRepuestos/ventaRepuestos.Entidades/Categoria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ventaRepuestos.Entidades
+{
+    public class Categoria
+    {
+        private int _codigo ;
+        private string _nombre;
+        
+        public Categoria(int codigo, string nombre)
+        {
+            _codigo = codigo;
+            _nombre = nombre;
+        }
+
+        public int Codigo
+        {
+            get { return _codigo; }
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+        }
+
+
+    }
+}
---
+++ ventaRepuestos/ventaRepuestos.Entidades/Repuesto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ventaRepuestos.Entidades
+{
+    public class Repuesto
+    {
+        private int _codigo;
+        private string _nombre;
+        private double _precio;
+        private int _stock;
+        private Categoria _categoria;
+
+        public int Codigo { get => _codigo; }
+        public string Nombre { get => _nombre; }
+        public double Precio { get => _precio; set => _precio = value;}
+        public int Stock { get => _stock; }
+        public Categoria Categoria { get => _categoria; }
+
+
+        public string ToString()
+        {
+            return this._nombre;
+        }
+
+    }
+}
